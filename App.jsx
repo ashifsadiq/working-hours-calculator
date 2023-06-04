7c6df6fc804303da8af7be8c35ec3ef2d0530675
ashifsadiq <ashifsadiq@gmail.com>
2023-06-04 20:38:42 +0530

    intiate
    initaiate the app for the testin purposes

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Dashboard from './Screens/Views/Dashboard'
+
+const App = () => {
+  return (
+    <View>
+      <Dashboard />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
